ea49be2094b1347911e29c70364294c5dc64d8b6
Netharus <nekromant12331@gmail.com>
2025-02-20 13:48:42 +0300

    Add repos

---
+++ product-service/src/main/java/com/modsen/productservice/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.modsen.productservice.repository;
+
+import com.modsen.productservice.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
