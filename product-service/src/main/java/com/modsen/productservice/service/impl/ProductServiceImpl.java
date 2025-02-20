c106edb72eb88d56535c8b12e5cbb18050bf6687
Netharus <nekromant12331@gmail.com>
2025-02-20 23:34:34 +0300

    Add product CRUD

--- product-service/src/main/java/com/modsen/productservice/service/impl/ProductServiceImpl.java
+++ product-service/src/main/java/com/modsen/productservice/service/impl/ProductServiceImpl.java
@@ -1,10 +1,14 @@
 package com.modsen.productservice.service.impl;
 
+import com.modsen.productservice.domain.Category;
 import com.modsen.productservice.domain.Product;
 import com.modsen.productservice.dto.PageContainerDto;
 import com.modsen.productservice.dto.ProductCreateDto;
 import com.modsen.productservice.dto.ProductResponseDto;
+import com.modsen.productservice.dto.ProductStandaloneCreateDto;
 import com.modsen.productservice.dto.ProductUpdateDto;
+import com.modsen.productservice.exception.ProductNotFound;
+import com.modsen.productservice.exception.ResourceAlreadyExistsException;
 import com.modsen.productservice.mapper.ProductMapper;
 import com.modsen.productservice.repository.ProductRepository;
 import com.modsen.productservice.service.ProductService;
@@ -22,37 +26,65 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
-
     private final ProductMapper productMapper;
 
     @Override
     public ProductResponseDto findById(Long id) {
-        return null;
+        return productMapper.toProductResponseDto(getProduct(id));
     }
 
     @Override
     public List<ProductResponseDto> findAll() {
-        return List.of();
+        return productMapper.toProductResponseDtoList(productRepository.findAll());
     }
 
     @Override
     public PageContainerDto<ProductResponseDto> findAll(Pageable pageable, String keyword) {
-        Page<Product> productPage = productRepository.findAll(pageable,keyword);
+        Page<Product> productPage = productRepository.findAll(pageable, keyword);
         return productMapper.toProductPageContainerDto(productPage);
     }
 
     @Override
-    public ProductResponseDto updateProduct(ProductUpdateDto productUpdateDto) {
+    public ProductResponseDto updateProduct(ProductUpdateDto productUpdateDto, Category category) {
+        Product updatedProduct = productMapper.fromProductUpdateDtoToProduct(productUpdateDto);
+
+        if (productRepository.existsByNameAndIdNot(updatedProduct.getName(), updatedProduct.getId())) {
+            throw new ResourceAlreadyExistsException("Product with name " + updatedProduct.getName() + " already exists");
+        }
+
+        Product product = getProduct(updatedProduct.getId());
+        product.setName(updatedProduct.getName());
+        product.setPrice(updatedProduct.getPrice());
+
+        if(updatedProduct.getCategory() != null) {
+            product.setCategory(category);
+        }
+        return productMapper.toProductResponseDto(productRepository.save(product));
+    }
+
+
+    @Override
+    public ProductResponseDto createProduct(ProductCreateDto productCreateDto, Category category) {
         return null;
     }
 
     @Override
-    public ProductResponseDto createProduct(ProductCreateDto productCreateDto) {
-        return null;
+    public ProductResponseDto createProduct(ProductStandaloneCreateDto productCreateDto, Category category) {
+        Product product = productMapper.fromProductStandaloneCreateDtoToProduct(productCreateDto);
+        if(productRepository.existsByName(product.getName()))
+            throw new ResourceAlreadyExistsException("Product with this name already exist " + product.getName());
+        product.setCategory(category);
+        return productMapper.toProductResponseDto(productRepository.save(product));
     }
 
+    //TODO Checking if product used in order
     @Override
     public void deleteProduct(Long id) {
 
     }
+
+    private Product getProduct(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new ProductNotFound("Product with id " + id + " not found"));
+    }
+
 }
