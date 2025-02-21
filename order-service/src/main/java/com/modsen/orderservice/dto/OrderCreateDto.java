d3e6e1e759ec3469071db2a236787977e753c4a2
Netharus <nekromant12331@gmail.com>
2025-02-21 13:17:39 +0300

    Update dtos

--- order-service/src/main/java/com/modsen/orderservice/dto/OrderCreateDto.java
+++ order-service/src/main/java/com/modsen/orderservice/dto/OrderCreateDto.java
@@ -1,11 +1,13 @@
 package com.modsen.orderservice.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record OrderCreateDto(
+        @NotNull(message = "User id can't be empty")
         Long userId,
         List<OrderItemCreateDto> orderItemsCreateDtoList
 ) {
