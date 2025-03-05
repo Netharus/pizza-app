6673e8fb217b0e6c5540e07d4d24fe85db322f9a
Netharus <nekromant12331@gmail.com>
2025-03-05 23:44:31 +0300

    Add api gateway work with Opaque token

--- api-gateway/src/main/java/com/modsen/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/modsen/apigateway/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.modsen.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.introspection.NimbusReactiveOpaqueTokenIntrospector;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 
@@ -15,14 +17,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableRedisWebSession
 public class SecurityConfig {
 
+    @Value("${spring.security.oauth2.resourceserver.opaque-token.introspection-uri}")
+    private String introspectionUri;
+    @Value("${spring.security.oauth2.resourceserver.opaque-token.client-id}")
+    private String clientId;
+    @Value("${spring.security.oauth2.resourceserver.opaque-token.client-secret}")
+    private String secret;
+
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http.authorizeExchange(auth -> auth
                         .anyExchange()
                         .authenticated())
                 .oauth2Login(withDefaults())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .opaqueToken(opaqueToken -> opaqueToken
+                                .introspector(new NimbusReactiveOpaqueTokenIntrospector(introspectionUri, clientId, secret))
+                        )
+
+                );
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
         return http.build();
     }
+
+
 }
