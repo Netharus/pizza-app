183b4c881866787ec6bf56a262a3cd3c66844afa
Netharus <nekromant12331@gmail.com>
2025-03-14 22:09:33 +0300

    Add const

--- user-service/src/main/java/com/modsen/userservice/dto/UsersCreateDto.java
+++ user-service/src/main/java/com/modsen/userservice/dto/UsersCreateDto.java
@@ -1,5 +1,6 @@
 package com.modsen.userservice.dto;
 
+import com.modsen.userservice.exceptions.ErrorMessages;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -8,23 +9,23 @@ import lombok.Builder;
 
 @Builder
 public record UsersCreateDto(
-        @NotBlank(message = "username can't be empty")
+        @NotBlank(message = ErrorMessages.USERNAME_CANNOT_BE_EMPTY)
         String username,
         @Email
         String email,
-        @NotBlank(message = "full name can't be empty")
+        @NotBlank(message = ErrorMessages.FULL_NAME_CANNOT_BE_EMPTY)
         String fullName,
         @Pattern(
-                regexp = "^\\+375(29|33|25)\\d{7}$",
-                message = "Phone number must be in the format +375XX0000000, where XX is 29, 33, or 25"
+                regexp = ErrorMessages.PHONE_REGEXP,
+                message = ErrorMessages.INCORRECT_PHONE_NUMBER
         )
         String phoneNumber,
 
-        @NotBlank(message = "Password cannot be empty")
-        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+        @NotBlank(message = ErrorMessages.PASSWORD_CANNOT_BE_EMPTY)
+        @Size(min = 8, max = 20, message = ErrorMessages.PASSWORD_SIZE_ERROR)
         @Pattern(
-                regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&)(#^])[A-Za-z\\d@$!%*?&]{8,}$",
-                message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+                regexp = ErrorMessages.PASSWORD_REGEXP,
+                message = ErrorMessages.INCORRECT_PASSWORD_FORMAT
         )
         String password
 ) {
